346c2ffa5008fbf56e447707f1bb3c10976433ab
Tamires <tamiresmanhaes4@gmail.com>
2019-06-14 15:00:04 -0300

    prova AvI 2017.2

---
+++ provas/AvI-2017.2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+void enunciado();
+
+int main(){
+    enunciado();
+
+    return 0;
+}
+
+void enunciado(){
+    printf("1) Crie um programa que tenha um vetor de caracteres. Esse vetor deve ter um tamanho máximo \n TAM = 10. Nenhum parâmetro pode ser global. Utilize boas práticas de modularização.\n O programa deve ter:\n");
+    printf("\n");
+    printf("A) a função main, responsável por interagir com o usuário, informando os resultados das operações das demais funções.\n");
+    printf("\n");
+    printf("B. uma função “insere caractere”. Ela deve solicitar o caractere ao usuário, e inserir no vetor, \n na primeira posição vazia,  se ainda tiver espaço. Deve retornar se foi inserido ou não.\n");
+    printf("\n");
+    printf("c. Uma função “troca caractere”. Essa função solicita um novo caractere ao usuário. A função deve procurar no vetor \n o primeiro caractere que é maior que o caractere informado, quando achar, fazera troca.\n A função deve retornar se houve a troca ou não. Quando houver a troca, a função principal \n(main) deve ser capaz de imprimir qual foi o caractere que saiu do vetor.\n");
+    printf("\n");
+    printf(" d. Uma função para remover caracteres. A remoção deve ser lógica, no final do vetor. Basta nesse caso decrementar o topo.\n");
+    printf("\n");
+    printf("e. Uma função para listar os caracteres.\n");
+    printf("\n");
+    printf("f. uma função que imprime cada um dos caracteres distintos e a quantidade de vezes que ele aparece no vetor.\n");
+    printf("\n");
+
+}
