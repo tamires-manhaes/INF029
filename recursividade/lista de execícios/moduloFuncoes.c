ca94f3e7d22a432a0d7f32018f315977be2c1ad3
Tamires Manhães <40744853+tamires-manhaes@users.noreply.github.com>
2019-09-25 20:12:05 -0300

    implementadas questões 2, 3 , 4

--- recursividade/lista de execícios/moduloFuncoes.c
+++ recursividade/lista de execícios/moduloFuncoes.c
@@ -19,3 +19,29 @@ int questao1(int n){
     else 
         return (n * questao1(n - 1));
 }
+
+int questao2 (int n){
+    if(n == 1 || n == 2)
+        return 1;
+    else 
+        return questao2(n-1) + questao2(n-2);
+}
+
+int questao3 (int n){
+    if(n == 0)
+      return n;
+    else {
+		printf("%d", n % 10);
+		n = n / 10;
+		return invert(n);
+    }
+}
+
+int questao4 (int *v, int tamanho){
+ if (tamanho == 1)
+      return v[0];
+   else 
+	  return v[tamanho - 1] + questao4(v, tamanho - 1); 
+}
+
+
