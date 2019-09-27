10e53967d5863ff6298b57e56454171a2f22d95b
Tamires Manhães <40744853+tamires-manhaes@users.noreply.github.com>
2019-09-27 19:32:40 -0300

    Update moduloFuncoes.c

--- recursividade/lista de execícios/moduloFuncoes.c
+++ recursividade/lista de execícios/moduloFuncoes.c
@@ -44,4 +44,27 @@ int questao4 (int *v, int tamanho){
 	  return v[tamanho - 1] + questao4(v, tamanho - 1); 
 }
 
+int questao12 (int n) {
+	if(n < 0) {
+		return 0;
+	}
+	naturaisCresc(n - 1);
+	printf("%d ", n);
+}
+
+int questao13 (int n) {
+	if(n == 0) {
+		return 0;
+	}
+	printf("%d ", n);
+	naturaisDec(n - 1);
+}
+
+int questao14 (int n){
+	if(n < 0) {
+		return 0;
+	}
+	2 + naturaisPares(n - 2);
+	printf("%d ", n);
+}
 
