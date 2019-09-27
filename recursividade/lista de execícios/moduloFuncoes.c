0787e99a19bb65215dc68581ea7d114df1e3d0a3
Tamires Manhães <40744853+tamires-manhaes@users.noreply.github.com>
2019-09-27 19:33:20 -0300

    Update moduloFuncoes.c

--- recursividade/lista de execícios/moduloFuncoes.c
+++ recursividade/lista de execícios/moduloFuncoes.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-#include "moduloFuncoes.h"
-
-int menu(){
-    int opcao; 
-    printf("\n");
-    printf("0 - Sair\n");
-    printf("1 - questao 1\n");
-    printf("\n");
-
-    printf("Digite opcao desejada: ");
-    scanf("%d", &opcao);
-    return opcao;
-}
 
 int questao1(int n){
     if((n == 1) || (n == 0))
